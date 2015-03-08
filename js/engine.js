2c5e166c0374cb1e12bc85cbd579dcd1de6042ae
stanchanpsu <stanchanpsu@outlook.com>
2015-03-07 23:48:54 -0500

    got coins working

--- js/engine.js
+++ js/engine.js
@@ -11,6 +11,26 @@ var grav = 1;
 var groundHeight;
 var ceilingHeight;
 
+var topran;
+var bottomran;
+var topprob;
+var bottomprob;
+var coinprob;
+var blockid = 0;
+var coinrowid = 0;
+var coinid = 0;
+var $obstacle;
+var $coin;
+var $coinrow;
+var numcoin;
+
+//colors
+
+var yellow = '#f3ff11';
+var blue = '#11edff';
+var green = '#23ff11';
+var pink = '#e811ff';
+
 $(document).ready(function(){
 	$body = $('body');
 	$game = $('#game');
@@ -22,7 +42,7 @@ $(document).ready(function(){
 	jump();
 	changecolor();
 	generateblock();
-	scrollBackground();
+//	genCoin();
 	
 	$(document).keyup(function(keypressed){
 		if(keypressed.which === 32){
@@ -34,27 +54,16 @@ $(document).ready(function(){
 	
 	setInterval(function(){
 		$('div.obstacle').each(function(){
-			collide($blockman, $(this));
-//			console.log($blockman.position().top);
-			
+			collide($blockman, $(this));			
         });
+//		console.log(grav);
+//		$('div.coin').each(function(){
+//			if ($(this).position().left <0){
+//				$(this).remove;
+//			}
+//		})
 	}, 150);
 });
-//
-//function slide(character, queue){
-//	$(document).keyup(function(keypressed){
-//		var key = keypressed.which;
-//		if (key == 40 && !queue){
-//			queue = true;
-//			character.animate({width: '120px', height: '50px'}, 100).delay(500)
-//				.animate({width: '100px', height: '100px'}, 100);
-//			window.setTimeout(function(){queue = false;}, 800);
-//		}
-//		else if(queue){
-//			return;
-//		}
-//	});
-//}
 
 function jump(){
     $(document).keyup(function(keypressed){
@@ -64,7 +73,7 @@ function jump(){
             
 			animatequeue = true;
 			
-            $blockman.animate({top: '-=200px', height: '100px', width: '90px'}, {duration: 200, ease: 'easeOutQuad', complete: function(){
+            $blockman.animate({top: '-=300px', height: '100px', width: '90px'}, {duration: 200, ease: 'easeOutQuad', complete: function(){
 				$blockman.animate({top: groundHeight - $blockman.height(), height: '100px', width: '100px'}, 0.8*groundHeight , 'easeInQuad', function(){
 					animatequeue = false;
 				});
@@ -77,7 +86,7 @@ function jump(){
             
 			animatequeue = true;
 			
-            $blockman.animate({top: '+=200px', height: '100px', width: '90px'}, {duration: 200, ease: 'easeOutQuad', complete: function(){
+            $blockman.animate({top: '+=300px', height: '100px', width: '90px'}, {duration: 200, ease: 'easeOutQuad', complete: function(){
 				$blockman.animate({top: ceilingHeight, height: '100px', width: '100px'}, 0.8*groundHeight , 'easeInQuad', function(){
 					animatequeue = false;
 				});
@@ -92,44 +101,48 @@ function changecolor(){
 	$(document).keyup(function(keypressed){
 		var key = keypressed.which;
 		if ( key == 39){
-			color = (color + 1) % 3;
+			color = (color + 1) % 4;
 		}
 		else if (key == 37){
-			color = (color + 2) % 3;
+			color = (color + 3) % 4;
 		}
 		switch(color){
 			case 0:
-				$blockman.animate({backgroundColor: '#11edff'}, {duration: 30, queue: false});
-				$ground.animate({backgroundColor: '#11edff'}, {duration: 30, queue: false});
-				$ceiling.animate({backgroundColor: '#11edff'}, {duration: 30, queue: false});
+				$blockman.animate({backgroundColor: blue}, {duration: 30, queue: false});
+				$ground.animate({backgroundColor: blue}, {duration: 30, queue: false});
+				$ceiling.animate({backgroundColor: blue}, {duration: 30, queue: false});
 				break;
 			case 1:
-				$blockman.animate({backgroundColor: '#8e00ac'}, {duration: 30, queue: false});
-				$ground.animate({backgroundColor: '#8e00ac'}, {duration: 30, queue: false});
-				$ceiling.animate({backgroundColor: '#8e00ac'}, {duration: 30, queue: false});
+				$blockman.animate({backgroundColor: pink}, {duration: 30, queue: false});
+				$ground.animate({backgroundColor: pink}, {duration: 30, queue: false});
+				$ceiling.animate({backgroundColor: pink}, {duration: 30, queue: false});
 				
 				break;
 			case 2:
-				$blockman.animate({backgroundColor: 'yellow'}, {duration: 30, queue: false});
-				$ground.animate({backgroundColor: 'yellow'}, {duration: 30, queue: false});
-				$ceiling.animate({backgroundColor: 'yellow'}, {duration: 30, queue: false});
+				$blockman.animate({backgroundColor: yellow}, {duration: 30, queue: false});
+				$ground.animate({backgroundColor: yellow}, {duration: 30, queue: false});
+				$ceiling.animate({backgroundColor: yellow}, {duration: 30, queue: false});
+				break;
+			case 3:
+				$blockman.animate({backgroundColor: green}, {duration: 30, queue: false});
+				$ground.animate({backgroundColor: green}, {duration: 30, queue: false});
+				$ceiling.animate({backgroundColor: green}, {duration: 30, queue: false});
+				break;
 		}
 	});
 }
 
 function generateblock(){
-    var ran;
-	var prob;
-	var blockid = 0;
-    var $obstacle;
-	var $coin;
     
     setInterval(function(){
 		
-		ran = Math.random();		
-		grav === 1 ? prob = 0.4 : prob = 0.6;		
+		topran = Math.random();	
+		bottomran = Math.random();
+		coinprob = Math.random();
+		grav === 1 ? (topprob = 0.5, bottomprob = 0.65) : (topprob = 0.65, bottomprob = 0.5);		
 		
-        if (ran < prob){
+		//generate top blocks
+        if (topran < topprob){
 			$obstacle = $("<div>", {id: "upblock" + blockid, class: "obstacle"});
             $game.append($obstacle.css({'background-color': window.ranColor(), 'top': '133px'}));
             $obstacle.css('right', '-200px').animate({right: '100vw'}, 3000, 'linear', function(){
@@ -137,14 +150,29 @@ function generateblock(){
             });
 		blockid++;
 			
-		$coin = $('<div>', {id: "coin" + blockid, class: "coin"});
-			$game.append($coin.css({'bottom': '133px'}));
-			$coin.css('right', '-200px').animate({right: '100vw'}, 3000, 'linear', function(){
-				$(this).remove();
-			});
-        }	
+		}
 		
-        if (ran > prob){
+		else if (topran > topprob && coinprob > 0.3){
+			
+			numcoin = window.ranInt(3,6);
+
+			for(i = 0; i < numcoin; i++){
+				$coin = $("<div>", {id: "coin" + coinid, class: "coin"});
+				$coin.css({'top': '133px', 'left': $('body').width() + 200 - i*66 + "px"}).appendTo($game).animate({left: 0 - i*66 + 'px'}, 3000, 'linear', function(){
+				$(this).remove();	
+				});
+				
+				coinid++;
+
+			}
+		}
+
+	}, 400+Math.random()*1000);
+	
+	setInterval(function(){
+		
+		//generate bottom blocks
+        if (bottomran < bottomprob){
 			$obstacle = $("<div>", {id: "downblock" + blockid, class: "obstacle"});
             $game.append($obstacle.css({'background-color': window.ranColor(), 'bottom': '133px'}));
             $obstacle.css('right', '-200px').animate({right: '100vw'}, 3000, 'linear', function(){
@@ -155,7 +183,7 @@ function generateblock(){
 			return;
 		}
 
-    }, 700+Math.random()*1000);
+    }, 400+Math.random()*1000);
 }
 
 function collide(elem1, elem2){
@@ -173,31 +201,23 @@ function collide(elem1, elem2){
     }
 }
 
-function scrollBackground(){
-    var x =0;
-    window.setInterval(function(){
-        $('#ground').css('backgroundPosition', x + 'px');
-        x-=5;
-    }, 10);
-}
-
 function ranInt(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function ranColor(){
-	switch(Math.floor(Math.random() * 3)){
+	switch(Math.floor(Math.random() * 4)){
 		case 0:
-			return '#11edff';
+			return green;
 			break;
 		case 1:
-			return '#8e00ac';
+			return blue;
 			break;
 		case 2:
-			return 'yellow';
+			return yellow;
 			break;
 		default:
-			return '#11edff';
+			return pink;
 	}
 }
 
@@ -219,3 +239,22 @@ function Gravity(thing){
 		
 	}, 1);
 }
+
+String.prototype.repeat = function( num )
+{
+    return new Array( num + 1 ).join( this );
+}
+//
+//function howManyCoins(){
+//	
+//	numcoin = ranInt(3,6);
+//	var coinarray = [];
+//
+//	for (i = 0; i < numcoin; i++){
+//		$coin = $("<div>", {id: "coin" + coinid, class: "coin"});
+//		coinarray.push($coin);
+//		coinid++;
+//	}
+//	
+//	return(coinarray);
+//}
